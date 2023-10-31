f63b489830d94576a3efa18fc743bdb84ad39865
shuichiro-hayafuji <shu1998hayafuji@gmail.com>
2023-11-01 01:24:30 +0900

    コードベース作成 first commmit

---
+++ app/client/src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    const ans = (async()=>{
+        const res = await fetch(`/api`);
+        return await res.json();
+    })()
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<hr>
+{#await ans}
+    <p>...loading</p>
+{:then data}
+    <h3>{data.message}</h3>
+    <h3>{data.status}</h3>
+{:catch error}
+    <p>An error occured!</p>
+{/await}
